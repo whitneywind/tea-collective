4c1aa4220cd642a78252b3cddada940b54a1deb4
whitneywind <78887901+whitneywind@users.noreply.github.com>
2023-07-02 13:49:16 -0700

    cart cards

--- src/components/MiniProductCard.tsx
+++ src/components/MiniProductCard.tsx
@@ -19,14 +19,14 @@ const ProductCard = ({ name, idName, image, desc, price, korName, korDesc, quant
                 <div className='col-md-8 p-4'>
                     <div id="mini-card-body" className="d-flex flex-column align-items-center align-items-md-end justify-content-center">
                         <p className="text-center fs-3">{langEng ? name : korName}</p>
-                        <p>Price: ${currTotalItemPrice}</p>
-                        <p>Qty: {currQuantity}</p>
-                        <div className='d-flex gap-5'>
-                            <button onClick={() => addToCart(product)} className=''>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
+                        <p>{langEng ? 'Price' : '가격'}: ${currTotalItemPrice}</p>
+                        <p>{langEng ? 'Qty' : '수'}: {currQuantity}</p>
+                        <div className='d-flex gap-2'>
+                            <button onClick={() => addToCart(product)} className='mini-card-btn'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-plus-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
                             </button>
-                            <button onClick={() => removeFromCart({ ...product, quantity: currQuantity })}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>
+                            <button onClick={() => removeFromCart({ ...product, quantity: currQuantity })} className='mini-card-btn'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-dash-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>
                             </button>
                         </div>
                     </div>
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -12,14 +12,14 @@ const Cart = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-arrow-left-square" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                 </svg>
-                <p className="d-none d-md-block fs-4">Back to Main Site</p>
+                <p className="d-none d-md-block fs-4">{langEng ? "Back to Main Site" : "홈으로 돌아가기"}</p>
             </Link>
             <h1 className="text-center fw-bold pt-4">{langEng ? "Shopping Cart" : "장바구니"}</h1>
             <div className="my-3 d-flex align-items-start flex-column align-items-end">
                 {products.map((product: Product, index: number) => (
                     <MiniProductCard {...product} key={index} />
                 ))}
-                <h2 className="w-100 pt-5 text-center">{langEng ? "Your cart total is: " : "총 합계: "} ${total}</h2>
+                <h2 className="w-100 pt-5 text-center">{langEng ? "Your cart total is: " : "총액: "} ${total}</h2>
             </div>
         </div>
      )
